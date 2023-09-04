1f536538bf44085581bc17e34563291064f2ce05
Roshin Jimmy <roshinjimmy14@gmail.com>
2023-09-04 17:58:56 +0530

    InnerHTML Added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-import { getDatabase, push, ref } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
+import {
+  getDatabase,
+  push,
+  ref,
+} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 
 const appSettings = {
   databaseURL: "https://grocify-3e577-default-rtdb.firebaseio.com/",
@@ -11,10 +15,14 @@ const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
-  push(shoppingListInDB, inputValue)
+  push(shoppingListInDB, inputValue);
+  inputFieldEl.value = "";
+
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
 
   console.log(inputValue);
 });
