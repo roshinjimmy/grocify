5b07f86a0f03623aa24ff6be3f22153000a4d0df
Roshin Jimmy <roshinjimmy14@gmail.com>
2023-09-04 13:40:22 +0530

    Firebase Database Added

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getDatabase, push, ref } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 
-addButtonEl.addEventListener("click",function(){
-    let inputValue = inputFieldEl.value
-    console.log(inputValue)
-})
+const appSettings = {
+  databaseURL: "https://grocify-3e577-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  push(shoppingListInDB, inputValue)
+
+  console.log(inputValue);
+});
